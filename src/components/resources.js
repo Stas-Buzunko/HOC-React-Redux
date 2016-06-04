b08d87350adec190d3061b9e1fcaed609bbd205d
Stas-Buzunko <buzunkostas@gmail.com>
2016-06-05 01:16:40 +0300

    initial commit

---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super special Recipe
+      <ul>
+        <li>React.JS</li>
+        <li>Redux</li>
+      </ul>
+    </div>
+  );
+}
